04d7cb59f0b90ffedd1aa93e5e9ec0d91d97db63
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-07-01 01:58:19 +0100

    Sexy but mostly unnecessary refactor

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,16 @@
-const wordCount = (inputText, minWordLen) => {
-  // RegEx searches for any special characters or capital letters (for camel case)
-  const wordArr = inputText.split(/[A-Z&\/\\ #,+()$~%.'\= ":;*?<>{}\-\n\|]/g);
-  const formattedArr = wordArr.filter(
-    (word) => word !== "" && word.length - 1 >= minWordLen
-  );
-  return formattedArr.length;
-};
-
 $(() => {
   $(".result-button").hide();
   $("#submit-button").on("click", (e) => {
     e.preventDefault();
 
     const inputText = $("#text-input").val();
-    const minLen = $("#length-input").val();
-    const result = wordCount(inputText, minLen);
+    const minWordLen = $("#length-input").val();
+    const result = inputText
+      // RegEx searches for any special characters or capital letters (for camel case)
+      .split(/[A-Z&\/\\ #,+()$~%.'\= ":;*?<>{}\-\n\|]/g)
+      // Then filtering array to remove empty strings and strings below min length
+      // then taking the length of the return
+      .filter((word) => word !== "" && word.length - 1 >= minWordLen).length;
     const resultStr =
       result === 1 ? `There is 1 word.` : `There are ${result} words.`;
 
