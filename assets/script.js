e99d35a381eb10515464822602ab95a8978d8e15
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-07-01 01:20:49 +0100

    Refactor

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,23 @@
 const wordCount = (inputText, wordLen) => {
   // RegEx searches for any special characters or capital letters (for camel case)
-  const wordArr = inputText.split(/[A-Z&\/\\ #,+()$~%.'\= ":;*?<>{}]/g);
-  console.log(wordArr);
-  const formattedArr = [];
-  wordArr.forEach((el) => {
-    const wordRules =
-      (el !== '' && el !== '\n' && el.length >= wordLen - 1) || el === 'a';
-
-    if (wordRules) formattedArr.push(el);
-  });
-  console.log(formattedArr);
-
+  const wordArr = inputText.split(/[A-Z&\/\\ #,+()$~%.'\= ":;*?<>{}\-\n\|]/g);
+  const formattedArr = wordArr.filter(
+    (word) => word !== "" && word.length >= wordLen - 1
+  );
   return formattedArr.length;
 };
 
 $(() => {
-  $('.result-button').hide();
-  $('#submit-button').on('click', (e) => {
-      e.preventDefault();
+  $(".result-button").hide();
+  $("#submit-button").on("click", (e) => {
+    e.preventDefault();
 
-      const inputText = $('#text-input').val();
-      const minLen = $('#length-input').val();
-      const result = wordCount(inputText, minLen);
- 
-      $('.result-button').text(`There are ${result} words.`);
-      $('.result-button').show(400);
+    const inputText = $("#text-input").val();
+    const minLen = $("#length-input").val();
+    const result = wordCount(inputText, minLen);
+    const resultStr =
+      result === 1 ? `There is 1 word.` : `There are ${result} words.`;
+
+    $(".result-button").hide(250).text(resultStr).show(250);
   });
 });
