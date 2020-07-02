5c39f4781d63ca224ac198a7cb3c18bd30478584
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-07-02 12:23:14 +0100

    Added replace() to maintain length of split camelcase words

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,15 @@ $(() => {
     const inputText = $("#text-input").val();
     const minWordLen = $("#length-input").val();
     const result = inputText
+      // Adding a placeholder with a space to split camel case words
+      .replace(/[A-Z]/g, " a")
       // RegEx searches for any special characters or capital letters (for camel case)
       .split(/[A-Z&\/\\ #,+()$~%.'\= ":;*?<>{}\-\n\|]/g)
       // Then filtering array to remove empty strings and strings below min length
       // then taking the length of the return
-      .filter((word) => word !== "" && word.length - 1 >= minWordLen).length;
+      .filter((word) => word !== "" && minWordLen <= word.length).length;
+    console.log(result);
+
     const resultStr =
       result === 1 ? `There is 1 word.` : `There are ${result} words.`;
 
